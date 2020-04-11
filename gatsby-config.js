96940e22cee5a3f173a18d420182df5bca830529
Hoang Trinh <hoang.trinhj@gmail.com>
2020-04-11 16:17:58 +0700

    Add Google Analytics Environment Variable

--- gatsby-config.js
+++ gatsby-config.js
@@ -84,7 +84,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID,
+        trackingId: process.env.GA_TRACKING_ID,
       },
     },
     {
